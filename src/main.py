66026073d7fd2c526ff3c31751a9752747c90db3
tqhoa <tqhoa8th@gmail.com>
2024-08-17 15:49:59 +0700

    upgradable fastapi verion lastest

--- src/main.py
+++ src/main.py
@@ -17,9 +17,9 @@ app = FastAPI(
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        # allow_origins=["*"],
-        allow_origins=[str(origin)
-                       for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=["*"],
+        # allow_origins=[str(origin)
+        #               for origin in settings.BACKEND_CORS_ORIGINS],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
