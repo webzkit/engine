49b20a2af2df679edf2e32e91749436812c9b770
tqhoa <tqhoa8th@gmail.com>
2024-10-24 00:26:38 +0700

    update schema user

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -39,17 +39,14 @@ class CreateUserSchema(UserBase):
     ]
 
 
-class UpdateUserSchema(UserBase):
-    email: Annotated[EmailStr, Field(examples=["info@zkit.com"], default=None)]
-    user_group_id: Annotated[int, Field(examples=[1], default=None)]
-    password: Annotated[
-        str,
-        Field(
-            pattern=r"^.{8,}|[0-9]+|[A-Z]+|[a-z]+|[^a-zA-Z0-9]+$",
-            examples=["Pa$$w0rd"],
-            default=None,
-        ),
+class UpdateUserSchema(BaseModel):
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    full_name: Annotated[
+        str | None,
+        Field(min_length=3, max_length=50, examples=["Full name"], default=None),
     ]
+    user_group_id: Annotated[int, Field(examples=[1])]
 
 
 class LoginForm(BaseModel):  # nopa
