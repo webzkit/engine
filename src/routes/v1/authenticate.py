fe42fb0e0b1f0301d74130308c1b2c48f69b3319
tqhoa <tqhoa8th@gmail.com>
2024-11-13 18:06:30 +0700

    add endpoint logout

--- src/routes/v1/authenticate.py
+++ src/routes/v1/authenticate.py
@@ -1,5 +1,5 @@
 from typing import Annotated, Any, Union
-from fastapi import Body, status, APIRouter, Depends, HTTPException
+from fastapi import Body, Header, Request, status, APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from crud.user import crud_user as crud
 from schemas.user import Login, UserRead as Read, UserReadLogin
@@ -8,6 +8,7 @@ from core.paginated.schemas import SingleResponse
 from routes.deps import async_get_db
 from models.group import Group
 from schemas.group import GroupRelationship
+from core.helpers.utils import parse_query_str
 
 
 router = APIRouter()
@@ -38,6 +39,33 @@ async def login(
     if not verify_password(data_request.password, dict(result)["hashed_password"]):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Password is wrong."
+        )
+
+    return {"data": result}
+
+@router.post(
+    "/logout", status_code=status.HTTP_200_OK, response_model=SingleResponse[Read]
+)
+async def logout(
+    request: Request,
+    db: Annotated[AsyncSession, Depends(async_get_db)],
+    request_init_data: Annotated[Union[str, None], Header()] = None,
+) -> Any:
+    current_user = parse_query_str(request_init_data)
+    id = int(current_user.get("id", 0))
+    result = await crud.get_joined(
+        db=db,
+        schema_to_select=Read,
+        id=id,
+        is_deleted=False,
+        join_model=Group,  # pyright: ignore
+        join_prefix="group_",
+        join_schema_to_select=GroupRelationship,
+    )
+
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
         )
 
     return {"data": result}
